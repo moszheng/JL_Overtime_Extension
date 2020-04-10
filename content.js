6d77d75a2f073d633ade3e1b6a64678590e852f4
Mos Zheng <47658172+moszheng@users.noreply.github.com>
2020-04-10 17:06:37 +0800

    JL_Overtime_Extension

---
+++ content.js
@@ -0,0 +1,7 @@
+console.log("內插腳本載入");
+
+var abc = document.getElementsByClassName("ol-xs-24 ico ico-clock -time");
+
+abc.append("Hello world");
+
+$(".ol-xs-24 ico ico-clock -time").append("Hello world")
